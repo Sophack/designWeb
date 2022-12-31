745fc956ef3049f79040158295b98a028afa969e
Saraf Nawar <s.999895831@gmail.com>
2022-12-31 09:22:40 -0500

    circle moves left to right without stopping

--- drawingApp/scriptD.js
+++ drawingApp/scriptD.js
@@ -3,6 +3,8 @@ const ctx = canvas.getContext('2d');
 
 //initial size: 
 let size = 30; 
+let x = 50;
+let y = 50;
 
 canvas.addEventListener('mousedown', () => {
 
@@ -16,8 +18,26 @@ function drawCircle(x,y) {
     ctx.fill();
 }
 
-//need a function that keeps drawing 
 
-drawCircle(75, 50);
+//below commented is no longer needed because x & y default values have been assigned
+// drawCircle(50, 50);
 
 //optional "counterclockwise" = False is default, and indicates clockwise, while true indicates counter-clockwise
+//need a function that keeps drawing 
+//below function is called every frame (draw circle always)
+
+
+function draw () {
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    //changed below to x & y values with ++ to increase by 1
+    //moves the circle from left to right 
+    drawCircle(x++,y);
+    
+    requestAnimationFrame(draw);
+}
+
+draw();
+
+//also need the rectangle to cling to the box
+//sources https://github.com/bradtraversy/vanillawebprojects/tree/master/breakout-game
+//initially the variables had number values assigned and then the default values were added after let size 
