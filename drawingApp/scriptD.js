a845c5cd88c143e3eda6aa73c942817c0740e92e
Saraf Nawar <s.999895831@gmail.com>
2023-01-02 14:49:33 -0500

    color & delete button working

--- drawingApp/scriptD.js
+++ drawingApp/scriptD.js
@@ -1,10 +1,10 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-const increaseBtn = document.getElementById("increase"); 
-const decreaseBtn = document.getElementById("decrease"); 
+const increaseBtn = document.getElementById("increase");
+const decreaseBtn = document.getElementById("decrease");
 const sizeEl = document.getElementById("size");
-const colorEl = document.getAnimations("color");
-const clearEl = document.getAnimations("clear");
+const colorEl = document.getElementById("color");
+const clearEl = document.getElementById("clear");
 
 //initial size:
 let size = 10;
@@ -13,18 +13,18 @@ let color = "black";
 let x = undefined;
 let y = undefined;
 
-canvas.addEventListener('mousedown', (e) => {
+canvas.addEventListener("mousedown", (e) => {
   isPressed = true;
 
   const x = e.offsetX;
   const y = e.offsetY;
 });
 
-canvas.addEventListener('mouseup', (e) => {
+canvas.addEventListener("mouseup", (e) => {
   isPressed = false;
 
   x = undefined;
-  y = undefined; 
+  y = undefined;
 });
 
 canvas.addEventListener("mousemove", (e) => {
@@ -33,12 +33,12 @@ canvas.addEventListener("mousemove", (e) => {
     const x2 = e.offsetX;
     const y2 = e.offsetY;
 
-    //function will pass both parameters 
+    //function will pass both parameters
     drawCircle(x2, y2);
     drawLine(x, y, x2, y2);
     x = x2;
-    y= y2;    
-}
+    y = y2;
+  }
 });
 
 function drawCircle(x, y) {
@@ -49,50 +49,49 @@ function drawCircle(x, y) {
   ctx.fill();
 }
 
-
-//function to delete drawing 
+//function to delete drawing
 drawLine(100, 100, 200, 200);
 
-function drawLine(x1, y1, x2, y2){
-    ctx.beginPath();
-    ctx.moveTo(x1, y1);
-    //add new point and creat a line to that point from the last specified point
-    ctx.lineTo(x2, y2);
-    ctx.strokeStyle = color;
-    ctx.lineWidth = size;
-    ctx.stroke();
+function drawLine(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  //add new point and creat a line to that point from the last specified point
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle = color;
+  ctx.lineWidth = size;
+  ctx.stroke();
 }
 
-increaseBtn.addEventListener('click', () => {
-        //when we increase we want: 
-        size += 5; 
-        //reset if its too big
-        if(size > 50) {
-            size=50;
-        }
-        updateSize();
+increaseBtn.addEventListener("click", () => {
+  //when we increase we want:
+  size += 5;
+  //reset if its too big
+  if (size > 50) {
+    size = 50;
+  }
+  updateSize();
 });
 
-decreaseBtn.addEventListener('click', () => {
-        //when size decreases we want 
-        size -= 5; 
-        if(size < 5) {
-            size=5;
-        }
-        updateSize();
+decreaseBtn.addEventListener("click", () => {
+  //when size decreases we want
+  size -= 5;
+  if (size < 5) {
+    size = 5;
+  }
+  updateSize();
 });
 
-//add function so color changes to corresponding user choice 
+//add function so color changes to corresponding user choice
 colorEl.addEventListener("change", (e) => {
-    color = e.target.value;
+  color = e.target.value;
 });
 
-clearEl.addEventListener("click", () =>{
-    ctx.clearRect(0, 0, canvas.clientWidth, canvas.height);
+clearEl.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
 
 function updateSize() {
-    sizeEl.innerText = size;
+  sizeEl.innerText = size;
 }
 
 //below commented is no longer needed because x & y default values have been assigned
